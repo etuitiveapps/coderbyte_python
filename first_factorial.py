7f46e4f9dd47396895600d6e541301a19d794f83
Etuitive Apps <namoricoe@gmail.com>
2015-05-10 22:17:13 -0500

    write test to pass 0 factorial

--- first_factorial.py
+++ first_factorial.py
@@ -1,12 +1,21 @@
 import unittest
 
 
+def first_factorial(input_int):
+    output_int = 1
+    if input_int == 0 or input_int == 1:
+        return 1
+    else:
+        return -1
+
+
+
 class FirstFactorialTest(unittest.TestCase):
 
     """ Test For ''first_factorial()'' function     """
 
     def test_first_reverse(self):
-        input_int = 4
-        output_int = 24
+        input_int = 0
+        output_int = 1
         result_int = first_factorial(input_int)
         self.assertEqual(result_int, output_int)
