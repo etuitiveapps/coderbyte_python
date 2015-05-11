0e5755cfec2660afabfd75895b22d6bc75fe1e85
Etuitive Apps <namoricoe@gmail.com>
2015-05-11 18:52:55 -0500

    Add text description of the letter capitalize problem

--- letter_capitalize.py
+++ letter_capitalize.py
@@ -1,6 +1,7 @@
 import unittest
 import re
 
+
 def letter_capitalize(input_string):
     string_list = re.split(r'(\s*)', input_string)
     output_list = []
@@ -12,7 +13,10 @@ def letter_capitalize(input_string):
 
 class LetterCapitalizeTest(unittest.TestCase):
 
-    """ Test For ''letter_capitalize()'' function     """
+    """ Test For ''letter_capitalize()'' function
+        Using the Python language, have the function LetterCapitalize(str) take the str parameter
+        being passed and capitalize the first letter of each word. Words will be separated by only one space.
+    """
 
     def test_letter_capitalize(self):
         input_string = 'hello world'
