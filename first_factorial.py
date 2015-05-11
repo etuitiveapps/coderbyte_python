9c78340d7e4906de4f511f781ddd315b504d8e7f
Etuitive Apps <namoricoe@gmail.com>
2015-05-10 22:44:55 -0500

    raise error for negative factorial

--- first_factorial.py
+++ first_factorial.py
@@ -3,7 +3,9 @@ import unittest
 
 def first_factorial(input_int):
     output_int = 1
-    if input_int == 0 or input_int == 1:
+    if input_int < 0:
+        raise ValueError('Invalid input: negative factorial detected')
+    elif input_int == 0:
         output_int = 1
     else:
         for current_number in range(1, input_int + 1):
@@ -32,3 +34,14 @@ class FirstFactorialTest(unittest.TestCase):
         output_int = 24
         result_int = first_factorial(input_int)
         self.assertEqual(result_int, output_int)
+
+    def test_eight_factorial(self):
+        input_int = 8
+        output_int = 40320
+        result_int = first_factorial(input_int)
+        self.assertEqual(result_int, output_int)
+
+    def test_negative_factorial(self):
+        input_int = -1
+        with self.assertRaises(ValueError):
+            first_factorial(input_int)
