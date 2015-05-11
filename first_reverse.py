c9e56323f5f9a05d40e9d65a6a42b9daaaeb2c31
Etuitive Apps <namoricoe@gmail.com>
2015-05-10 21:18:17 -0500

    implement the first reverse method to pass first test

--- first_reverse.py
+++ first_reverse.py
@@ -1,8 +1,16 @@
 import unittest
 
 
+def first_reverse(input_string):
+    output_list = []
+    character_list = list(input_string)
+    for character in reversed(character_list):
+        output_list.append(character)
+    return ''.join(output_list)
+
 
 class FirstReverseTest(unittest.TestCase):
+
     """ Test for the ''first_reverse()'' function     """
 
     def test_first_reverse(self):
